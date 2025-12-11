eade2b1cb3de3717b1206c8435b40f8b425b8ee3
Babita Kumari <babitakumari.ky3007@gmail.com>
2025-12-11 14:25:23 +0530

    generalized the deployed and developement BASE_URL logically

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,4 @@
-// const BASE_URL="https://codedsa.onrender.com/api/v1/"
-const BASE_URL = "http://localhost:3000/api/v1/"
+const BASE_URL = import.meta.env.VITE_DEVELOPMENT ? "http://localhost:3000/api/v1/" : "https://codedsa.onrender.com/api/v1/"
 
 export const authEndpoints = {
     LOGIN_API: BASE_URL + "auth/login",
